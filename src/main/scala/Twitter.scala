41a971cada75d9f98022bfa610ec5b50ed2e1824
Amlan Alok <amlanalok@gmail.com>
2022-04-24 01:59:39 -0500

    code done with spark.sql

--- src/main/scala/Twitter.scala
+++ src/main/scala/Twitter.scala
@@ -8,18 +8,48 @@ object Twitter {
   def main ( args: Array[ String ]): Unit = {
     val conf = new SparkConf().setAppName("Twitter")
     val spark = SparkSession.builder().config(conf).getOrCreate()
+
+    /** This import is needed to use the $-notation */
     import spark.implicits._
 
     /* Reading data from data source */
 //    val inputData = spark.read.csv(args(0))
 ////      .as[Follows]
 
-    val in = spark.sparkContext.textFile(args(0))
+    val inputDF = spark.sparkContext.textFile(args(0))
       .map(_.split(",")).map(x => Follows(x(0).toInt, x(1).toInt)).toDF()
 
-    in.show()
-    in.printSchema()
+    println("-----------"+inputDF.getClass)
+    inputDF.show()
+    inputDF.printSchema()
 
+//    val x1 = inputDF.groupBy("follower").count()
+//    x1.show()
+//
+//    val x2 = x1.withColumnRenamed("count", "follower_count")
+//    x2.show()
+//
+//    val x3 = x2.groupBy("follower_count").count()
+//    x3.show()
+
+    inputDF.createOrReplaceTempView("X")
+
+    val output1 = spark.sql("" +
+      "SELECT x.follower, count(x.user) no_of_users_followed " +
+      "FROM X x " +
+      "GROUP BY x.follower")
+    println("-----------"+output1.getClass)
+    output1.show()
+
+    output1.createOrReplaceTempView("Y")
+
+    val output2 = spark.sql("" +
+      "SELECT y.no_of_users_followed, count(y.follower) user_count " +
+      "FROM Y y " +
+      "GROUP BY y.no_of_users_followed " +
+      "ORDER BY y.no_of_users_followed")
+
+    output2.show()
 
 
 
